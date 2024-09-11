07aa84db9bd8caae4ea38d9f4627af7ea023611e
yaroslav <20459871+dusaniuk@users.noreply.github.com>
2024-09-12 00:44:56 +0300

    feat: show pets count

--- src/components/petsoft/dashboard/stats.tsx
+++ src/components/petsoft/dashboard/stats.tsx
@@ -1,7 +1,17 @@
+"use client";
+
+import { usePetContext } from "~/context/pet-context";
+
 export function Stats() {
+  const { pets } = usePetContext();
+
+  const totalPets = pets.length;
+
   return (
     <section className="text-center">
-      <p className="text-2xl font-bold leading-6">2</p>
+      <p className="text-2xl font-bold leading-6">
+        {totalPets}
+      </p>
       <p className="opacity-80">Current guests</p>
     </section>
   );
